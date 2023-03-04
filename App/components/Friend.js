20d3f8f445fdcaa8b774502cc5acc099c1a40985
sg-fernando <fernando.sandoval.garza@gmail.com>
2023-03-04 16:24:46 -0700

    node modules

--- App/components/Friend.js
+++ App/components/Friend.js
@@ -1,10 +1,12 @@
-import { View, Text, Pressable } from 'react-native'
+import { View, Text, Pressable, FlatList } from 'react-native'
 import React from 'react'
 
 const Friend = (props) => {
   return (
     <Pressable>
-      <Text>{props.name}</Text>
+      <FlatList 
+        data={}
+      />
     </Pressable>
   )
 }
