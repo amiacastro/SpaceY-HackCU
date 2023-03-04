4761fcb233c04bff6d377c69afe093f76ddfcea2
sg-fernando <fernando.sandoval.garza@gmail.com>
2023-03-04 16:31:42 -0700

    friend

--- App/constants/friends.js
+++ App/constants/friends.js
@@ -2,22 +2,18 @@ const FriendData = [
   {
     id: 1,
     name: "Jessica Tan",
-    image: require("../assets/person02.png"),
   },
   {
     id: 2,
     name: "Jennifer Sia",
-    image: require("../assets/person03.png"),
   },
   {
     id: 3,
     name: "Rosie Wong",
-    image: require("../assets/person04.png"),
   },
   {
     id: 4,
     name: "Vincent Swift",
-    image: require("../assets/person05.png"),
   }
 ]
-export { NFTData };
+export { FriendData };
