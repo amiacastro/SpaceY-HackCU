41dac4d396bb49a97e0927c2c65d8ec536a444f6
amiacastro <acastro@mines.edu>
2023-03-04 16:15:39 -0700

    idk

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { Button, View, SafeAreaView, FlatList, Text } from 'react-native';
 import { COLORS } from '../constants';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 import { HomeHeader, FocusedStatusBar } from '../componenets';
 
