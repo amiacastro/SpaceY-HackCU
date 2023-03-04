7ef0e539a394ef7d43dc19263024467b4fcd340f
sg-fernando <fernando.sandoval.garza@gmail.com>
2023-03-04 15:04:00 -0700

    screens

---
+++ App/screens/FriendScreen.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const FriendScreen = () => {
+    return (
+        <View style={styles.container}>
+        <Text>FriendScreen</Text>
+        </View>
+    );
+}
+
+export default FriendScreen;
