494b808faca8bfeecf03d76a80cabd788fea16f3
Albert <aphan@mines.edu>
2023-03-04 15:02:18 -0700

    oijoijfew;oijf;oij

---
+++ App/screens/Home.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { View, SafeAreaView, FlatList, Text } from 'react-native';
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
