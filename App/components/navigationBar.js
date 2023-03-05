c7c96e1a55daa8dc2e683826a15e9e1301238bf0
amiacastro <acastro@mines.edu>
2023-03-04 17:33:40 -0700

    stuff

---
+++ App/components/navigationBar.js
@@ -0,0 +1,8 @@
+import { View, Text, StatusBar } from 'react-native'
+import React from 'react'
+
+export const navigationBar = () => {
+  return (
+    <div>navigationBar</div>
+  )
+}
--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,16 +1,13 @@
 import { useState } from 'react';
 import { Button, View, SafeAreaView, FlatList, Text } from 'react-native';
 import { COLORS } from '../constants';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { HomeHeader, FocusedStatusBar } from '../components';
 
 const Home = ({navigation}) => {
   return (
     <SafeAreaView style={{ flex:1 }}>
-      <FocusedStatusBar background={COLORS.primary} />
-
       <View style={{ flex: 1}}>
         <View style={{zIndex: 0}}>
           <Text>This is the Home Page</Text>
