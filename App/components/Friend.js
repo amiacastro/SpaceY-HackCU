30031365004c7bc1dce46d90c4b8f7711ea3049d
sg-fernando <fernando.sandoval.garza@gmail.com>
2023-03-04 17:17:43 -0700

    friends

--- App/components/Friend.js
+++ App/components/Friend.js
@@ -4,9 +4,15 @@ import React from 'react'
 const Friend = (props) => {
   const friend = props.friend
   return (
-    <View>
-      <Text>Hi</Text>
-    </View>
+    <Pressable>
+      <View style={{flexDirection: 'row', alignItems: 'center', padding: 10}}>
+        <Image source={friend.image} style={{width: 50, height: 50, borderRadius: 25}} />
+        <View style={{marginLeft: 10}}>
+          <Text style={{fontSize: 18, fontWeight: 'bold'}}>{friend.name}</Text>
+          <Text style={{fontSize: 14, color: 'gray'}}>{friend.user}</Text>
+        </View>
+      </View>
+    </Pressable>
   )
 }
 
--- App/screens/Friends.js
+++ App/screens/Friends.js
@@ -1,14 +1,14 @@
-import { FlatList, SafeAreaView, Text, View } from 'react-native';
-import Friend from '../components';
-import FriendData from '../constants';
+import { Button, FlatList, SafeAreaView, Text, View } from 'react-native';
+import { Friend } from '../components';
+import { FriendData } from '../constants';
 
 const Friends = ({navigation}) => {
     return (
-        <SafeAreaView>
-            <Text>Friends</Text>
+        <SafeAreaView style={{ flex:1 }}>
+            <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
             <FlatList 
                 data={FriendData}
-                renderItem={({item}) => <Text>TEDNJSANDASKJDSA</Text>}
+                renderItem={({item}) => <Friend friend={item} />}
                 keyExtractor={item => item.id}
             />
         </SafeAreaView>
