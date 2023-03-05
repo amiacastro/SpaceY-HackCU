a2388751fcabc0ce40094ee127ee1a26e8723835
amiacastro <acastro@mines.edu>
2023-03-04 23:06:46 -0700

    cjafds

--- App/screens/AppScreens.js
+++ App/screens/AppScreens.js
@@ -2,13 +2,9 @@ import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from './Home';
-<<<<<<< HEAD
 import Friends from './Friends'
 import Calendar from './Calendar';
-=======
-import Friends from './Friends';
 import Profile from './Profile';
->>>>>>> 4bccd97a2ac87d850836f41e9f471c5d7bcf0690
 
 const Tab = createBottomTabNavigator();
 
